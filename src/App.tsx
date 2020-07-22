bb271580b042af2df3aff6c5d7a37d9efeef25a3
龚箭 <15031617367@163.com>
2020-07-22 21:30:08 +0800

    优化后的导航栏你

--- src/App.tsx
+++ src/App.tsx
@@ -8,62 +8,32 @@ import {
     Link,
     Redirect
 } from "react-router-dom";
-import Nav from "./components/Nav";
-
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
-
-
+import Tag from "./views/Tags";
+import Money from "./views/Money";
+import Statistics from "./views/Statistics";
+import NoMatch from "./views/NoMatch";
 
 
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tag">
-                            <Tag />
-                        </Route>
-                        <Route path="/money">
-                            <Money />
-                        </Route>
-                        <Route path="/statistics">
-                            <Statistics />
-                        </Route>
-                        <Redirect exact from="/" to='/money' />
-                        <Route path="*">
-                            <NoMatch />
-                        </Route>
-                    </Switch>
-                </Main>
-                <Nav></Nav>
-            </Wrapper>
+            <Switch>
+                <Route path="/tag">
+                    <Tag />
+                </Route>
+                <Route path="/money">
+                    <Money />
+                </Route>
+                <Route path="/statistics">
+                    <Statistics />
+                </Route>
+                <Redirect exact from="/" to='/money' />
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
         </Router>
     );
-}
-function NoMatch(){
-    return <h2>404,页面不存在</h2>
-}
-
-function Tag() {
-    return <h2>标签页</h2>;
-}
-
-function Money() {
-    return <h2>记账页</h2>;
-}
-
-function Statistics() {
-    return <h2>统计页</h2>;
 }
 
 export default App;
---
+++ src/views/Tags.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import React from "react";
+
+function Tag() {
+    return (
+        <Layout>
+            <h2>标签页</h2>
+        </Layout>
+    )
+}
+export  default  Tag
