a6ed0760f840df0aa7e6fa37638f506b9ed52739
李承斌 <15031617367@163.com>
2020-07-23 17:20:03 +0800

    Money.tsx之 TagsSection UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,31 @@ import Statistics from "./views/Statistics";
 import NoMatch from "./views/NoMatch";
 
 
+const AppWrapper =styled.div`
+  color:#333
+`
+
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/tag">
-                    <Tag />
-                </Route>
-                <Route path="/money">
-                    <Money />
-                </Route>
-                <Route path="/statistics">
-                    <Statistics />
-                </Route>
-                <Redirect exact from="/" to='/money' />
-                <Route path="*">
-                    <NoMatch />
-                </Route>
-            </Switch>
-        </Router>
+        <AppWrapper>
+            <Router>
+                <Switch>
+                    <Route path="/tag">
+                        <Tag />
+                    </Route>
+                    <Route path="/money">
+                        <Money />
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics />
+                    </Route>
+                    <Redirect exact from="/" to='/money' />
+                    <Route path="*">
+                        <NoMatch />
+                    </Route>
+                </Switch>
+            </Router>
+        </AppWrapper>
     );
 }
 
