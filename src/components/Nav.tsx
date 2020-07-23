a81c2ca7dab621133e963c5e6b7825d68427faf0
李承斌 <15031617367@163.com>
2020-07-23 16:51:10 +0800

    图表高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import {Link,NavLink} from "react-router-dom";
 import React from "react";
 import Icon from "./icon";
 
@@ -19,7 +19,13 @@ const NavWrapper =styled.div`
           .icon{
             width: 24px;
             height: 24px;
-          }  
+          }
+          &.selected{
+            color: red;
+            .icon{
+              fill: red;
+            }
+          } 
       }
     }    
   }
@@ -29,23 +35,23 @@ const Nav = ()=>{
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="/tag">
+                    <NavLink to="/tag" activeClassName="selected">
                         <Icon name="tag" />
                         标签页
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
 
-                    <Link to="/money">
+                    <NavLink to="/money" activeClassName="selected">
                         <Icon name="money" />
                         记账页
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/statistics">
+                    <NavLink to="/statistics" activeClassName="selected">
                         <Icon name="chars" />
                         统计页
-                    </Link>
+                    </NavLink>
                 </li>
             </ul>
         </NavWrapper>
