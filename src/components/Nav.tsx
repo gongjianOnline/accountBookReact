0f6523999507c7bfe79eb4c6283adee97ef14721
龚箭 <15031617367@163.com>
2020-07-23 20:35:18 +0800

    Money.tsx之tagsSection UI 完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,8 @@ import {Link,NavLink} from "react-router-dom";
 import React from "react";
 import Icon from "./icon";
 
-const NavWrapper =styled.div`
+const NavWrapper =styled.nav`
+  background: #fff;
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
