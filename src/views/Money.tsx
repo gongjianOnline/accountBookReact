c21e478a57cb7a7a61e11dba1be3bc3157cff46f
龚箭 <15031617367@163.com>
2020-07-26 14:30:01 +0800

    完成备注的功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import React from "react";
 import styled from "styled-components";
 import {TagsSection} from "./Money/TagsSection";
-import {NotesSection} from "./Money/NotesSection";
+import {NoteSection} from "./Money/NoteSection";
 import {CategorySection} from "./Money/CategorySection";
 import {NumberPadSection} from "./Money/NumberPadSection";
 
@@ -17,12 +17,8 @@ function Money() {
         <Mylayout>
             <TagsSection>
             </TagsSection>
-            <NotesSection>
-                <label>
-                    <span>备注</span>
-                    <input type="text"/>
-                </label>
-            </NotesSection>
+            <NoteSection>
+            </NoteSection>
             <CategorySection>
                 <ul>
                     <li className="selected">支出</li>
