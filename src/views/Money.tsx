740d103c5eecf7841c15a15167b11c982f0abe72
龚箭 <15031617367@163.com>
2020-07-26 10:11:18 +0800

    完成标签的选择和新增

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -15,14 +15,7 @@ function Money() {
     // @ts-ignore
     return (
         <Mylayout>
-            <TagsSection className="tags">
-                <ol>
-                    <li>衣</li>
-                    <li>食</li>
-                    <li>住</li>
-                    <li>行</li>
-                </ol>
-                <button>新增标签</button>
+            <TagsSection>
             </TagsSection>
             <NotesSection>
                 <label>
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React,{useState} from "react";
 
-const TagsSection = styled.section`
+const Weapper = styled.section`
   background: #fff; 
   padding: 12px 16px;
   flex-grow: 1;
@@ -17,6 +18,10 @@ const TagsSection = styled.section`
         padding: 3px 18px;
         font-size: 14px;
         margin: 8px 12px;
+        &.selected{
+          background: #f60;
+          color: #fff;
+        }
     }
   }
   >button{
@@ -27,5 +32,42 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `
+
+const TagsSection:React.FC = (props)=>{
+    const [tags,setTages] = useState<string[]>(["衣","食","住","行"])
+    const [selectTages,setSelectTages] = useState<string[]>([])
+    const onAddTag = ()=>{
+        const tageName =window.prompt("新标签的名称为")
+        if(tageName){
+            setTages([...tags,tageName])
+        }
+    }
+    const ontoggleTag = (time:string)=>{
+        const index = selectTages.indexOf(time)
+        if(index >=0){
+            //如果tag以被选中,就复制所有备选中tag,作为新的selectedTag
+            setSelectTages(selectTages.filter(t=>{return t !== time}))
+        }else{
+            setSelectTages([...selectTages,time])
+        }
+    }
+    const X = (item:string)=>{
+        return selectTages.indexOf(item)>=0?'selected':''
+    }
+    return(
+        <Weapper>
+            <ol>
+                {tags.map((time)=>{
+                    return <li key={time}
+                               onClick={()=>ontoggleTag(time)}
+                               className={X(time)}>
+                            {time}
+                            </li>
+                })}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Weapper>
+    )
+}
 
 export  {TagsSection}
