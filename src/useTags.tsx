12c568a7f53d429bcd3b28fd41cc6bbe6d4bcd10
李承斌 <15031617367@163.com>
2020-08-04 17:33:34 +0800

    下午提交

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -26,15 +26,23 @@ const useTags = ()=>{ //封装自定义Hooks
         //把tagsClone的第index删掉,换成{id:id,name:obj.name}
         tagsClone.splice(index,1,{id:id,name:obj.name})
         setTages(tagsClone)
-
+    }
+    const deleteTag = (id:number) =>{
+        //获取要删除的下标
+        const index = findTagIndex(id)
+        //深拷贝tags得到tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags))
+        //把tagsClone的第index删掉
+        tagsClone.splice(index,1)
+        setTages(tagsClone)
     }
     return {
         tags,
         setTages,
         findTag,
         updateTag,
-        findTagIndex
+        findTagIndex,
+        deleteTag
     }
 }
-
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -26,31 +26,38 @@ const InputWrapper = styled.div`
 `
 
 const Tag:React.FC = ()=>{
-    const {findTag,updateTag} = useTags();
+    const {findTag,updateTag,deleteTag} = useTags();
     let {id:idString} = useParams<Params>()
     const tag = findTag(parseInt(idString));
-    return (
-        <Layout>
-            <Topbar>
-                <Icon name="left"/>
-                <span>编辑标签</span>
-                <Icon/>
-            </Topbar>
-            <InputWrapper>
-                <Input label="标签名"
-                       placeholder="标签页"
-                       value={tag.name}
-                        onChange={(e)=>{
-                            updateTag(tag.id,{name:e.target.value})
-                        }}/>
-            </InputWrapper>
-            <Center>
-                <Space/>
-                <Space/>
-                <Button>刪除标签</Button>
-            </Center>
-        </Layout>
+        return (
+            <Layout>
+                <Topbar>
+                    <Icon name="left"/>
+                    <span>编辑标签</span>
+                    <Icon/>
+                </Topbar>
+                {tag?<div>
+                    <InputWrapper>
+                        <Input label="标签名"
+                               placeholder="标签页"
+                               value={tag.name}
+                               onChange={(e)=>{
+                                   updateTag(tag.id,{name:e.target.value})
+                               }}/>
+                    </InputWrapper>
+                    <Center>
+                        <Space/>
+                        <Space/>
+                        <Button onClick={()=>{deleteTag(tag.id)}}>刪除标签</Button>
+                    </Center>
+                </div>:<Center>
+                    tag不存在
+                </Center>}
 
-    )
+
+            </Layout>
+
+        )
+
 }
 export {Tag}
