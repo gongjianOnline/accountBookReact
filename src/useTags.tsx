89bcbc15a0c8329203c7d26844310b7e47ec82fa
李承斌 <15031617367@163.com>
2020-08-04 16:59:40 +0800

    编辑tag名称

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -8,11 +8,32 @@ const defaultTage = [
 ]
 const useTags = ()=>{ //封装自定义Hooks
     const [tags,setTages] = useState<{id:number;name:string}[]>(defaultTage)
-    const findTag = (id:number) => tags.filter(tag=>tag.id === id)[0]
+    const findTag = (id:number) => tags.filter(tag=>tag.id === id)[0];
+    const findTagIndex = (id:number) =>{
+        let result = -1
+        for(let i=0;i<tags.length;i++){
+            if(tags[i].id === id){
+                result = i
+                break;
+            }
+        }
+        return result;
+    }
+    const updateTag = (id:number,obj:{name:string}) =>{
+        const index = findTagIndex(id)
+        //深拷贝tags得到tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags))
+        //把tagsClone的第index删掉,换成{id:id,name:obj.name}
+        tagsClone.splice(index,1,{id:id,name:obj.name})
+        setTages(tagsClone)
+
+    }
     return {
         tags,
         setTages,
-        findTag
+        findTag,
+        updateTag,
+        findTagIndex
     }
 }
 
