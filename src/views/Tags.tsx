8bac15e888f748c4c88ac984b661bf70e9885688
李承斌 <15031617367@163.com>
2020-08-04 15:31:24 +0800

    tag页面详情布局完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -4,6 +4,7 @@ import {useTags} from "../useTags";
 import styled from "styled-components";
 import Icon from "../components/icon";
 import {Link} from "react-router-dom";
+import {Button} from "../components/Button";
 
 
 const Taglist = styled.ol`
@@ -20,15 +21,6 @@ const Taglist = styled.ol`
            align-items: center;  
         }
       }
-`
-const Button = styled.button`
-  font-size: 18px;
-  border: none;
-  padding: 8px 12px;
-  background: #767676;
-  border-radius: 4px;
-  color: #fff;
-  cursor:pointer;
 `
 const Center = styled.div`
   display:flex;
